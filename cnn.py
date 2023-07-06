8ee082cef427db54ba7806228d9b7e5d9d67f037
Zuba13 <116648055+Zuba13@users.noreply.github.com>
2023-07-06 18:59:09 +0200

    try

--- cnn.py
+++ cnn.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models
+from keras.regularizers import l2
 from matplotlib import pyplot as plt
 
 EPOCHS = 100
 BATCH_SIZE = 16
-MODEL_EPOCHS = [3, 6, 9]
-NAMES = ["model1", "model2", "model3", "ultimate_model"]
+MODEL_EPOCHS = [5, 10, 5, 10, 5, 10]
+NAMES = ["model1", "model2", "best_model1", "best_model2", "worst_model1", "worst_model2"]
 DATA_FILE_NAME = "./train.csv"
 
 def load_data_from_csv(file_path):
@@ -29,8 +30,33 @@ def create_model(input_shape, num_classes, dropout_rate):
         layers.MaxPooling2D((2, 2)),
         layers.Flatten(),
         layers.Dense(64, activation="relu"),
-        layers.Dropout(dropout_rate),
         layers.Dense(num_classes, activation="softmax")
+    ])
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+def create_worse_model(input_shape, num_classes, dropout_rate):
+    model = models.Sequential([
+        layers.Conv2D(filters=32, kernel_size=(5,5), activation="relu", input_shape=input_shape),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(num_classes, activation="softmax")
+    ])
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+def build_model2(input_shape, num_classes):
+    model = models.Sequential([
+        layers.Conv2D(64, (3, 3), activation='relu', input_shape=input_shape, kernel_regularizer=l2(0.0001)),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(256, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation='softmax')
     ])
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     return model
@@ -56,18 +82,8 @@ def plot_training(history_list, model_names):
         model_name = model_names[i]
 
         plt.figure(figsize=(6, 4))
-        plt.plot(history.history['loss'], label='Training Loss')
-        plt.plot(history.history['val_loss'], label='Validation Loss')
-        plt.title('{} Loss'.format(model_name))
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.legend()
-        plt.tight_layout()
-        plt.show()
-
-        plt.figure(figsize=(6, 4))
-        plt.plot(history.history['accuracy'], label='Training Accuracy')
-        plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+        plt.plot(np.arange(1, len(history.history['accuracy']) + 1), history.history['accuracy'], label='Training Accuracy')
+        plt.plot(np.arange(1, len(history.history['val_accuracy']) + 1), history.history['val_accuracy'], label='Validation Accuracy')
         plt.title('{} Accuracy'.format(model_name))
         plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
@@ -75,30 +91,37 @@ def plot_training(history_list, model_names):
         plt.tight_layout()
         plt.show()
 
+
+
 if __name__ == "__main__":
     data = load_data_from_csv(DATA_FILE_NAME)
 
-    train_data = data[:40000]
-    test_data = data[40000:]
+    train_split = 0.7
+    val_split = 0.2
+    test_split = 0.1
+
+    split_index_train = int(len(data) * train_split)
+    split_index_val = int(len(data) * (train_split + val_split))
+
+    train_data = data[:split_index_train]
+    val_data = data[split_index_train:split_index_val]
+    test_data = data[split_index_val:]
 
     train_vectors, train_labels = preprocess_data(train_data)
+    val_vectors, val_labels = preprocess_data(val_data)
     test_vectors, test_labels = preprocess_data(test_data)
 
-    validation_split = 0.2
-    split_index = int(len(train_vectors) * (1 - validation_split))
-    validation_vectors, validation_labels = train_vectors[split_index:], train_labels[split_index:]
-    train_vectors, train_labels = train_vectors[:split_index], train_labels[:split_index]
 
     trained_models = []
     predictions_list = []
     history_list = []
 
-    for i, model_file_name in enumerate(NAMES[:-1]):
+    for i, model_file_name in enumerate(NAMES[:-2]):
         model_epochs = MODEL_EPOCHS[i]
         model_batch_size = BATCH_SIZE * (EPOCHS // model_epochs)
 
         model = create_model(input_shape=(28, 28, 1), num_classes=10, dropout_rate=0.2*(i+1))
-        history = train_model(model, train_vectors, train_labels, validation_vectors, validation_labels, model_epochs, model_batch_size)
+        history = train_model(model, train_vectors, train_labels, val_vectors, val_labels, model_epochs, model_batch_size)
         save_model(model, model_file_name)
         trained_models.append(model)
 
@@ -111,19 +134,42 @@ if __name__ == "__main__":
 
         history_list.append(history)
 
-    ultimate_model = create_model(input_shape=(28, 28, 1), num_classes=10, dropout_rate=0.5)
-    ultimate_history = train_model(ultimate_model, train_vectors, train_labels, validation_vectors, validation_labels, EPOCHS, BATCH_SIZE)
-    save_model(ultimate_model, NAMES[-1])
-    trained_models.append(ultimate_model)
+    for i, model_file_name in enumerate(NAMES[-2:]):
+        model_epochs = MODEL_EPOCHS[i]
+        model_batch_size = BATCH_SIZE * (EPOCHS // model_epochs)
 
-    ultimate_model = load_model(NAMES[-1])
-    ultimate_predictions = predict_labels(ultimate_model, test_vectors)
-    predictions_list.append(ultimate_predictions)
+        model = build_model2(input_shape=(28, 28, 1), num_classes=10)
+        history = train_model(model, train_vectors, train_labels, val_vectors, val_labels, model_epochs, model_batch_size)
+        save_model(model, model_file_name)
+        trained_models.append(model)
 
-    ultimate_accuracy = get_accuracy(test_labels, ultimate_predictions)
-    print("Ultimate Model: Accuracy: {:.2f}%".format(ultimate_accuracy * 100))
+        model = load_model(model_file_name)
+        predictions = predict_labels(model, test_vectors)
+        predictions_list.append(predictions)
 
-    history_list.append(ultimate_history)
+        accuracy = get_accuracy(test_labels, predictions)
+        print("Best Model {}: Accuracy: {:.2f}%".format(i+1, accuracy * 100))
 
-    model_names = ["Model 1", "Model 2", "Model 3", "Ultimate Model"]
+        history_list.append(history)
+    
+    for i, model_file_name in enumerate(NAMES[-2:]):
+        model_epochs = MODEL_EPOCHS[i]
+        model_batch_size = BATCH_SIZE * (EPOCHS // model_epochs)
+
+        model = create_worse_model(input_shape=(28, 28, 1), num_classes=10, dropout_rate=0.2*(i+1))
+        history = train_model(model, train_vectors, train_labels, val_vectors, val_labels, model_epochs, model_batch_size)
+        save_model(model, model_file_name)
+        trained_models.append(model)
+
+        model = load_model(model_file_name)
+        predictions = predict_labels(model, test_vectors)
+        predictions_list.append(predictions)
+
+        accuracy = get_accuracy(test_labels, predictions)
+        print("Best Model {}: Accuracy: {:.2f}%".format(i+1, accuracy * 100))
+
+        history_list.append(history)
+
+    model_names = ["Model 1", "Model 2", "Best Model 1", "Best Model 2", "Worst Model 1" , "Worst Model 2"]
+    model_names = NAMES[:len(history_list)]
     plot_training(history_list, model_names)
